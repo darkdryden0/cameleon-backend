e4e45b6e84e1784780b339904f348a346083c6b0
kui <kui@mhub24.com>
2025-04-16 16:43:52 +0800

    회원관련 내용추가

--- src/Controller/MemberController.php
+++ src/Controller/MemberController.php
@@ -26,8 +26,8 @@ class MemberController extends BaseController
     {
         $param = $this->getContentParams();
         // 이메일 사용가능한지 확인
-        $emailFlag = $this->memberService->checkEmail($param);
-        if (!$emailFlag) return $this->response('이미 가입한 이메일 입니다.', [], Response::HTTP_BAD_REQUEST);
+        $userFlag = $this->memberService->checkUserId($param);
+        if (!$userFlag) return $this->response('이미 가입한 아이디 입니다.', [], Response::HTTP_BAD_REQUEST);
 
         // 회원가입 성공했는지 확인
         $result = $this->memberService->registerMember($param);
@@ -41,7 +41,7 @@ class MemberController extends BaseController
     {
         $param = $this->getContentParams();
         $result = $this->memberService->checkLogin($param);
-        if (!$result) return $this->response('회원 등록 실패.', [], Response::HTTP_BAD_REQUEST);
+        if (!$result) return $this->response('아이디 또는 비밀번호를 확인해주세요.', [], Response::HTTP_BAD_REQUEST);
 
         return $this->response('success', []);
     }
@@ -51,5 +51,25 @@ class MemberController extends BaseController
     {
         $mallInfo = $this->memberService->getMemberInfo();
         return $this->response('success', $mallInfo);
+    }
+
+    #[Route('/api/modify/member', methods: 'PUT')]
+    public function modifyMember(): Response
+    {
+        $param = $this->getContentParams();
+        $result = $this->memberService->modifyMember($param);
+        if ($result) return $this->response($result, [], Response::HTTP_BAD_REQUEST);
+
+        return $this->response('success', []);
+    }
+
+    #[Route('/api/change/password', methods: 'PUT')]
+    public function changePassword(): Response
+    {
+        $param = $this->getContentParams();
+        $result = $this->memberService->changePassword($param);
+        if ($result) return $this->response($result, [], Response::HTTP_BAD_REQUEST);
+
+        return $this->response('success', []);
     }
 }
--- src/Service/MemberService.php
+++ src/Service/MemberService.php
@@ -10,24 +10,24 @@ use Psr\Log\LoggerInterface;
 class MemberService
 {
     protected LoggerInterface $appLogger;
-    private MemberInfoRepository $memberLoginRepository;
+    private MemberInfoRepository $memberInfoRepository;
 
     public function __construct(
-        LoggerInterface       $appLogger,
-        MemberInfoRepository $memberLoginRepository
+        LoggerInterface      $appLogger,
+        MemberInfoRepository $memberInfoRepository
     )
     {
         $this->appLogger = $appLogger;
-        $this->memberLoginRepository = $memberLoginRepository;
+        $this->memberInfoRepository = $memberInfoRepository;
     }
 
-    public function checkEmail($param): bool
+    public function checkUserId($param): bool
     {
         $where = [
-            'email' => ArrayUtil::getVal('email', $param),
+            'user_id' => ArrayUtil::getVal('user_id', $param),
         ];
-        $dbResult = $this->memberLoginRepository->count($where);
-        // 이메일이 이미 존재한다면 실패 리턴
+        $dbResult = $this->memberInfoRepository->count($where);
+        // 아이디가 이미 존재한다면 실패 리턴
         if ($dbResult > 0) return false;
         return true;
     }
@@ -35,11 +35,16 @@ class MemberService
     public function registerMember($param): bool
     {
         $data = [
+            'user_id' => ArrayUtil::getVal('user_id', $param),
             'email' => ArrayUtil::getVal('email', $param),
             'password' => ArrayUtil::getVal('password', $param),
-            'mall_id' => Context::getMallId(),
+            'phone' => ArrayUtil::getVal('phone', $param),
+            'company' => ArrayUtil::getVal('company', $param),
+            'business_num' => ArrayUtil::getVal('business_num', $param),
+            'credit' => 0,
+            'create_date' => date('Y-m-d H:i:s'),
         ];
-        $dbResult = $this->memberLoginRepository->insert($data);
+        $dbResult = $this->memberInfoRepository->insert($data);
         // 처리결과 -1이면 실패 리턴
         if ($dbResult === -1) return false;
         return true;
@@ -48,11 +53,10 @@ class MemberService
     public function checkLogin($param): bool
     {
         $where = [
-            'email' => ArrayUtil::getVal('email', $param),
+            'user_id' => ArrayUtil::getVal('user_id', $param),
             'password' => ArrayUtil::getVal('password', $param),
-            'mall_id' => Context::getMallId(),
         ];
-        $dbResult = $this->memberLoginRepository->count($where);
+        $dbResult = $this->memberInfoRepository->count($where);
         // 검색결과 1개이하면 실패 리턴
         if ($dbResult < 1) return false;
         return true;
@@ -60,9 +64,68 @@ class MemberService
 
     public function getMemberInfo(): ?array
     {
-        $userInfo = $this->memberLoginRepository->findOneBy(['user_id' => Context::getUserId()]);
+        $userInfo = $this->memberInfoRepository->findOneBy(['user_id' => Context::getUserId()]);
 
         if (ArrayUtil::isValidArray($userInfo)) return $userInfo;
         return [];
+    }
+
+    public function getMemberByEmail($email): array
+    {
+        return $this->memberInfoRepository->findBy(['email' => $email]);
+    }
+
+    public function modifyMember($param): string
+    {
+        $updateData = [
+            'email' => ArrayUtil::getVal('email', $param),
+            'phone' => ArrayUtil::getVal('phone', $param),
+            'company' => ArrayUtil::getVal('company', $param),
+            'business_num' => ArrayUtil::getVal('business_num', $param),
+        ];
+
+        $password = ArrayUtil::getVal('password', $param);
+        // 비번 입력했을 경우에 처리한다.
+        if ($password) {
+            $checkPwd = ArrayUtil::getVal('check_pwd', $param);
+            // 비번 체크하여 에러메세지 있으면 리턴한다.
+            $checkResult = $this->checkPassword($password, $checkPwd);
+            if ($checkResult) return $checkResult;
+            // 에러메세지 없으면 비번수정해야 한다.
+            $updateData['password'] = $password;
+        }
+
+        $memberResult = $this->memberInfoRepository->update($updateData, ['user_id' => Context::getUserId()]);
+        if ($memberResult < 1) return '계정 정보 수정 실패';
+        return '';
+    }
+
+    public function changePassword($param): string
+    {
+        $password = ArrayUtil::getVal('password', $param);
+        $checkPwd = ArrayUtil::getVal('check_pwd', $param);
+        // 비번 체크하여 에러메세지 있으면 리턴한다.
+        $checkResult = $this->checkPassword($password, $checkPwd);
+        if ($checkResult) return $checkResult;
+
+        $memberResult = $this->memberInfoRepository->update(['password' => $password], ['user_id' => Context::getUserId()]);
+        if ($memberResult < 1) return '비밀번호 수정 실패';
+        return '';
+    }
+
+    private function checkPassword($password, $checkPwd): string
+    {
+        if (!$password) return '비밀번호를 입력해 주세요.';
+
+        // 비번 정규식 체크
+        if (preg_match('/^(?=.*[A-Za-z])(?=.*\d)(?=.*[^A-Za-z\d]).{8,}$/', $password) === false) {
+            return '비밀번호는 8자 이상의 영어, 숫자, 특수문자 조합으로 설정해 주세요.';
+        }
+
+        // 두번 입력한 비번이 다를 경우 실패
+        if ($password !== $checkPwd) {
+            return '비밀번호가 정확하지 않습니다.';
+        }
+        return '';
     }
 }
